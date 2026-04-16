4c1d9462f12542df38353759ea8bfe4800afa054
Diksha Pimpalkar <diksha.pimpalkar@gmail.com>
2026-04-16 18:42:30 +0530

    Implement quicksort algorithm in quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        // code here
+        if(low>=high) return;
+        int index= partition(arr, low, high);
+        quickSort(arr, low, index-1);
+        quickSort(arr, index+1, high);
+    }
+
+  public:
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while(i<j) {
+            while(arr[i]<=pivot && i<=high-1) {
+                i++;
+            }
+            while(arr[j]> pivot && j>=low+1) {
+                j--;
+            }
+            if(i<j) swap(arr[i], arr[j]);
+        }
+        swap(arr[low], arr[j]);
+        return j;
+        // code here
+    }
+};
